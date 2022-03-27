43c87e8e5f06701d138c93efd35bbb90e782f3c9
Aleksey <92379825+Yaran66@users.noreply.github.com>
2022-03-27 17:40:06 +0300

    Add files via upload

--- parser.c
+++ parser.c
@@ -11,7 +11,12 @@
 /* ************************************************************************** */
 #include "includes/so_long.h"
 
-enum e_errors	ft_check_map()
+enum e_errors	ft_check_map(t_map *map, char *str)
+{
+
+}
+
+static void ft_check_flags(t_map *map, enum e_errors *error)
 
 enum e_errors	map_parser(int fd, t_map *map)
 {
@@ -23,8 +28,10 @@ enum e_errors	map_parser(int fd, t_map *map)
 	str = get_next_line(fd);
 	map->size_x = ft_strlen(str);
 	if(str)
-		err = ft_check_map() // TODO check the map for correct symbols, size, ..
-	// TODO BAD_WALLS, adelina
+		err = ft_check_map(map, str) // TODO check the map for correct symbols,
+				// size, ..
+	if (!error && !map->flags.is_wall) // TODO BAD_WALLS, adelina, ХЗ пока зачем
+		error = MAP_BAD_WALLS;
 	while (err == NO_ERROR && str)
 	{
 		tmp = map->field;
@@ -36,6 +43,8 @@ enum e_errors	map_parser(int fd, t_map *map)
 		err = ft_check_map()
 	}
 	free(str);
-	ft_check_flags() // TODO check flags, in case if flags is 0 (missing
+	ft_check_flags(map, &err) // TODO check flags, in case if flags is 0
+	// (missing
 	// wall, exit, player (or too many players)
+	return (err);
 }
